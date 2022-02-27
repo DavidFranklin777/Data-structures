f5596ec156467a404139668c1c53637ee60e3332
DavidFranklin <davidfranklin777@gmail.com>
2022-02-27 21:38:52 +0100

    LinkedLists: Introduction of removing functions

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Node
 {
 public:
-    int data;                   // integer data that the linked list must hold
+    int data;                   // integer data that the linked list can hold
     Node *next;                 // this pointer holds the value of the next node
 
     /*
@@ -13,10 +13,9 @@ public:
      * @param _data
      *
      */
-
-    // The constructor basically initializes the two elements of a linked list, i.e data and next
+    // The constructor initializes the two elements of a linked list, i.e data and next pointer
     // This is necessary, otherwise the tail of the linkedlist may not have an ending point which would result in an infinite sweep while reading a linkedlist
-    Node (int _data)
+    Node (int _data = 0)
     {
         data = _data;
         next =  NULL;
@@ -33,7 +32,6 @@ public:
      * @param tail   - This points to the last block of the linked list
      * @size         - Size of the linkedlist
      */
-
     LinkedLists()
     {
         header = NULL;
@@ -44,12 +42,20 @@ public:
     Node *header;
     Node *tail;
     int   size;
+    int  getSize();
     void appendData(int data);
     void prependData(int data);
     void printList();
+    void removeFirstElement();
+    void removeLastElement();
+    void removeAt(int pos);
 };
 
- void LinkedLists::appendData(int data)
+int LinkedLists::getSize()
+{
+    return size;
+}
+void LinkedLists::appendData(int data)
  {
      // Create a new node
      Node *newElement = new Node(data);
@@ -59,7 +65,6 @@ public:
         header = newElement;
         tail  = newElement;
      }
-
      // Case 2 List is not empty
      else
      {
@@ -68,7 +73,6 @@ public:
      }
      // Increment size of the linkedlist
      size++;
-
  }
 
  void LinkedLists::prependData(int data)
@@ -76,14 +80,12 @@ public:
      //Create a new node
      Node *newElement = new Node(data);
      Node *temp;
-
      // Case 1 Empty list
      if(NULL == header)
      {
          header = newElement;
          tail = newElement;
      }
-
      // Case 2 list not empty
      else
      {
@@ -91,32 +93,107 @@ public:
          header = newElement;
          newElement->next = temp;
      }
-
      size++;
  }
 
  void LinkedLists::printList()
  {
     Node *temp = header;
-
+    cout << "The linked list of size " << size << " is:\n";
     while (NULL != temp)
     {
-        cout << " " << temp->data;
+        cout << temp->data << " ";
         temp = temp->next;
     }
-
     cout << endl;
  }
 
+ void LinkedLists::removeFirstElement()
+ {
+     // Case 1: List is empty
+     // Nothing to delete as there is no list
 
+     // Case 2: List is not empty
+     // Create a temp node and assign header to it, advance header and delete the temp.
 
+     if (NULL != header)
+     {
+         Node *temp = header;
+         header = header->next;
+         delete temp;
+     }
+
+     else
+     {
+         cout << "List is empty. Nothing to delete!\n";
+     }
+
+     size--;
+ }
+
+ void LinkedLists::removeLastElement()
+ {
+    // Case 1: Empty list
+     if(NULL == header)
+     {
+         cout << "List is empty. Nothing to remove!\n";
+     }
+     else if (NULL == header->next)
+     {
+         removeFirstElement();
+     }
+
+     else
+     {
+         // Create two nodes cur and prev
+        Node *cur = header;
+        Node *prev;
+        while (NULL != cur->next)
+        {
+            prev = cur;
+            cur = cur->next;
+        }
+        tail = prev;
+        prev->next = NULL;
+        delete cur;
+        size--;
+     }
+
+ }
+
+ void LinkedLists::removeAt(int pos)
+ {
+    if (pos > size)
+    {
+        cout << "Position greater than size, Invalid\n";
+        return;
+    }
+
+    else if (NULL != header)
+    {
+        Node *cur = header;
+        Node *prev;
+        for (int i =1; i<pos; i++)
+        {
+            prev = cur;
+            cur = cur->next;
+        }
+        prev->next = cur->next;
+        delete cur;
+        size--;
+    }
+ }
 int main()
 {
     LinkedLists list;
     list.appendData(1);
     list.appendData(2);
     list.appendData(3);
+    list.appendData(4);
     list.prependData(10);
+//    list.removeFirstElement();
+//    list.removeLastElement();
+    list.removeAt(2);
     list.printList();
 
 
